6244ebf3afdfe7fcce9f1b943c58f57210d3babd
alexey.zh <mailbox.stdio@gmail.com>
2025-06-06 17:45:52 +0500

    Brew formula update for kubectl-apidocs version v1.0.14

--- kubectl-apidocs.rb
+++ kubectl-apidocs.rb
@@ -5,21 +5,21 @@
 class KubectlApidocs < Formula
   desc "Quickly navigate and inspect Kubernetes API resources with an interactive tree view"
   homepage "https://github.com/hashmap-kz/homebrew-tap"
-  version "1.0.12"
+  version "1.0.14"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/hashmap-kz/kubectl-apidocs/releases/download/v1.0.12/kubectl-apidocs_v1.0.12_darwin_amd64.tar.gz"
-      sha256 "dd3031af14af489d9da5399f71401d28986d383a25493d40b74bef71b4ba4f0f"
+      url "https://github.com/hashmap-kz/kubectl-apidocs/releases/download/v1.0.14/kubectl-apidocs_v1.0.14_darwin_amd64.tar.gz"
+      sha256 "cedeb77757e75afe78bcf6acc02225a8418d6f3870c7860a3db13eaecb455e4c"
 
       def install
         bin.install "kubectl-apidocs"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/hashmap-kz/kubectl-apidocs/releases/download/v1.0.12/kubectl-apidocs_v1.0.12_darwin_arm64.tar.gz"
-      sha256 "baf2e79c2233a78ea69a3425afe73a288809433cd38b58040e14b386e9469510"
+      url "https://github.com/hashmap-kz/kubectl-apidocs/releases/download/v1.0.14/kubectl-apidocs_v1.0.14_darwin_arm64.tar.gz"
+      sha256 "5f947c38a966bd862eac3a06bcf8894f29728a01922f7e47ad8aff3d3b86756c"
 
       def install
         bin.install "kubectl-apidocs"
@@ -28,24 +28,18 @@ class KubectlApidocs < Formula
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      if Hardware::CPU.is_64_bit?
-        url "https://github.com/hashmap-kz/kubectl-apidocs/releases/download/v1.0.12/kubectl-apidocs_v1.0.12_linux_amd64.tar.gz"
-        sha256 "0456615d9329dc80af1bead3aa9c90fc5229e6c36afc4ed1c90f7b691b2371e5"
-
-        def install
-          bin.install "kubectl-apidocs"
-        end
+    if Hardware::CPU.intel? and Hardware::CPU.is_64_bit?
+      url "https://github.com/hashmap-kz/kubectl-apidocs/releases/download/v1.0.14/kubectl-apidocs_v1.0.14_linux_amd64.tar.gz"
+      sha256 "1a2d8ff5fff738d6410a23b0b97b13e660a51558e702e067eab03572b5cad9b2"
+      def install
+        bin.install "kubectl-apidocs"
       end
     end
-    if Hardware::CPU.arm?
-      if Hardware::CPU.is_64_bit?
-        url "https://github.com/hashmap-kz/kubectl-apidocs/releases/download/v1.0.12/kubectl-apidocs_v1.0.12_linux_arm64.tar.gz"
-        sha256 "ce533512fab8669a2e22208ea989669c44f5156be6fe30886b9a887a9b8f46f3"
-
-        def install
-          bin.install "kubectl-apidocs"
-        end
+    if Hardware::CPU.arm? and Hardware::CPU.is_64_bit?
+      url "https://github.com/hashmap-kz/kubectl-apidocs/releases/download/v1.0.14/kubectl-apidocs_v1.0.14_linux_arm64.tar.gz"
+      sha256 "9d1597c349a375182cbb4e8027fd6470c239e02c92ce1f66776bf170b92c1b83"
+      def install
+        bin.install "kubectl-apidocs"
       end
     end
   end
